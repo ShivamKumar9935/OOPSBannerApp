83df7104a78dbd5373cbdbd91e2a87b787015aa4
Shivam Kumar <codedpilott@gmail.com>
2026-02-16 14:15:59 +0530

    Enhance banner output in OOPSBannerApp

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,20 @@
-
-
 public class OOPSBannerApp {
 
-   
+    /**
+     * Main method - Entry point of application
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
 
-       
-        System.out.println("OOPS");
+        System.out.println(" *****   *****   *****    ***** ");
+        System.out.println("*     * *     * *     *  *     *");
+        System.out.println("*     * *     * *     *  *      ");
+        System.out.println("*     * *     * *****     ******");
+        System.out.println("*     * *     * *              *");
+        System.out.println("*     * *     * *              ");
+        System.out.println(" *****   *****   *          ***** ");
+
+
 
     }
 }

564c63cde52b82ef0b7f9f2b4f265c23c4492ee2
Shivam Kumar <codedpilott@gmail.com>
2026-02-16 13:43:34 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,10 @@
-/**
- * OOPSBannerApp
- * 
- * Use Case 1 (UC1): Print the literal text "OOPS" to the console.
- * 
- * This is the foundational step of the OOPS Banner App project.
- * 
- * @author Shivam
- * @version 1.0
- */
+
 public class OOPSBannerApp {
 
-    /**
-     * Main method - Entry point of the application
-     * @param args Command line arguments
-     */
+   
     public static void main(String[] args) {
 
-        // Printing OOPS text to console
+       
         System.out.println("OOPS");
 
     }
